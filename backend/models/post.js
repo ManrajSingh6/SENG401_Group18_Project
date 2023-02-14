cc2a97901085cc7355fcaa572ab2e418db8fac44
Aarushi Roychoudhury <91914220+aarushirc14@users.noreply.github.com>
2023-02-14 14:08:45 -0700

    Initial schema for post

--- backend/models/post.js
+++ backend/models/post.js
@@ -1,1 +1,28 @@
 // MongoDB model for Posts
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+    username: {type:String,required:true},
+
+    title: {type:String,required:true},
+
+    body: {type:String,required:true},
+
+    time: {
+        type: Date,
+        default: Date.now()
+    },
+    type: String,
+    link: String,
+
+    votes: {
+        type: Number,
+        default: 0
+    },
+    numOfComments: {
+        type: Number,
+        default: 0
+    }
+});
+
+module.exports = mongoose.model('Post', postSchema);

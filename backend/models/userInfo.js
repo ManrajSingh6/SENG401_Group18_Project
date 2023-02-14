a596ae99e92cb4860a2b5081dcd1dd7bf687c18a
Aarushi Roychoudhury <91914220+aarushirc14@users.noreply.github.com>
2023-02-14 14:09:21 -0700

    intial schema for user

--- backend/models/userInfo.js
+++ backend/models/userInfo.js
@@ -1,1 +1,26 @@
 // MongoDB Model for user information
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    created: Date,
+    savedPosts: Array,
+    savedComments: Array,
+    subscribed: Array,
+    owned: Array
+});
+
+module.exports = mongoose.model('User', userSchema);

9fe6beb8c38cfb7c157f493749b62786a9668650
faricamago <farica.mago@ucalgary.ca>
2023-03-22 19:01:32 -0600

    new folder

--- frontend/src/tests/App.test.js
+++ frontend/src/tests/App.test.js
@@ -1,6 +1,6 @@
 import {render,screen} from '@testing-library/react';
 import {BrowserRouter} from 'react-router-dom';
-import App from './App';
+import App from '../components/App';
 import CreatePostPage from "../pages/createPost";
 import '@testing-library/jest-dom';
 

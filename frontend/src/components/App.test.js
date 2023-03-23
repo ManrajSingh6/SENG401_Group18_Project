eca66cc581ea5688b216e2dcd71352f5a0fda732
faricamago <farica.mago@ucalgary.ca>
2023-03-22 18:48:17 -0600

    All tests for App.jsx file

--- frontend/src/components/App.test.js
+++ frontend/src/components/App.test.js
@@ -4,6 +4,7 @@ import App from './App';
 import CreatePostPage from "../pages/createPost";
 import '@testing-library/jest-dom';
 
+// create-post page in App.jsx
 jest.mock('../pages/createPost', () => {
     const CreatePostPage = () => (
       <div data-testid='testCreatePostPage'>Hello World</div>
@@ -21,7 +22,146 @@ jest.mock('../pages/createPost', () => {
    );
 
   expect(screen.getByTestId("testCreatePostPage")).toBeInTheDocument();
-   //const linkElement = screen.getByText(/learn react/i);
-   //expect(linkElement).toBeInTheDocument();
  });
 
+//login page router in App.jsx
+ jest.mock('../pages/loginPage', () => {
+  const LoginPage = () => (
+    <div data-testid='testLoginPage'>Hello World</div>
+  );
+
+  return LoginPage;
+});
+
+test('renders LoginPage', () => {
+  window.history.pushState({},"","/login");
+ render(
+  <BrowserRouter>
+      <App/>
+  </BrowserRouter>
+ );
+
+expect(screen.getByTestId("testLoginPage")).toBeInTheDocument();
+});
+
+
+//Register page router in App.jsx
+jest.mock('../pages/registerPage', () => {
+  const RegisterPage = () => (
+    <div data-testid='testRegisterPage'>Hello World</div>
+  );
+
+  return RegisterPage;
+});
+
+test('renders RegisterPage', () => {
+  window.history.pushState({},"","/register");
+ render(
+  <BrowserRouter>
+      <App/>
+  </BrowserRouter>
+ );
+
+expect(screen.getByTestId("testRegisterPage")).toBeInTheDocument();
+});
+
+//edit post page router in App.jsx
+jest.mock('../pages/editPost', () => {
+  const EditPost = () => (
+    <div data-testid='testEditPost'>Hello World</div>
+  );
+
+  return EditPost;
+});
+
+test('renders EditPost', () => {
+  window.history.pushState({},"","/edit-post/:id");
+ render(
+  <BrowserRouter>
+      <App/>
+  </BrowserRouter>
+ );
+
+expect(screen.getByTestId("testEditPost")).toBeInTheDocument();
+});
+
+//edit thread page router in App.jsx
+jest.mock('../pages/editThread', () => {
+  const EditThread = () => (
+    <div data-testid='testEditThread'>Hello World</div>
+  );
+
+  return EditThread;
+});
+
+test('renders EditThread', () => {
+  window.history.pushState({},"","/edit-thread/:name");
+ render(
+  <BrowserRouter>
+      <App/>
+  </BrowserRouter>
+ );
+
+expect(screen.getByTestId("testEditThread")).toBeInTheDocument();
+});
+
+//profile page router in App.jsx
+jest.mock('../pages/profilePage', () => {
+  const ProfilePage = () => (
+    <div data-testid='testProfilePage'>Hello World</div>
+  );
+
+  return ProfilePage;
+});
+
+test('renders ProfilePage', () => {
+  window.history.pushState({},"","/user-profile/:username");
+ render(
+  <BrowserRouter>
+      <App/>
+  </BrowserRouter>
+ );
+
+expect(screen.getByTestId("testProfilePage")).toBeInTheDocument();
+});
+
+//thread page router in App.jsx
+jest.mock('../pages/threadPage', () => {
+  const ThreadPage = () => (
+    <div data-testid='testThreadPage'>Hello World</div>
+  );
+
+  return ThreadPage;
+});
+
+test('renders ThreadPage', () => {
+  window.history.pushState({},"","/:threadName/");
+ render(
+  <BrowserRouter>
+      <App/>
+  </BrowserRouter>
+ );
+
+expect(screen.getByTestId("testThreadPage")).toBeInTheDocument();
+});
+
+
+//post view page router in App.jsx
+jest.mock('../pages/postView', () => {
+  const PostView = () => (
+    <div data-testid='testPostView'>Hello World</div>
+  );
+
+  return PostView;
+});
+
+test('renders PostView', () => {
+  window.history.pushState({},"","/:threadName/post/:postID");
+ render(
+  <BrowserRouter>
+      <App/>
+  </BrowserRouter>
+ );
+
+expect(screen.getByTestId("testPostView")).toBeInTheDocument();
+});

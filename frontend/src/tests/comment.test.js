d690ba9d3525fa35dea256189ab80e65a0c75488
faricamago <farica.mago@ucalgary.ca>
2023-03-25 18:52:22 -0600

    more tests

--- frontend/src/tests/comment.test.js
+++ frontend/src/tests/comment.test.js
@@ -5,7 +5,7 @@ import ThumbDownIcon from '@mui/icons-material/ThumbDownOffAlt';
 import Comment from '../components/comment';
 import '@testing-library/jest-dom';
 import { render, screen, fireEvent } from '@testing-library/react';
-//import 'jsdom-global/register';
+
 //testing like comment
 test('renders thumbUpIcon', () => {
   const testme = renderer.create(<ThumbUpIcon />).toJSON();
@@ -34,13 +34,3 @@ const button = screen.getByTestId('delcommentbutton', {name: "delete-comment-lin
 fireEvent.click(button);
 expect(screen.getByTestId("delcommentbutton")).toBeInTheDocument();
 });
-
-// //testing if image upload is working properly
-// it('should call any passed in onError after an image load error', () => {
-//   const img = {}
-//   global.document.createElement = (type) => type === 'img' ? img : null
-//   const onError = jest.fn();
-//   mount(<Image {...props} src={"crap.junk"} onError={onError} />);
-//   img.onload();
-//   expect(onError).toHaveBeenCalled();
-// });

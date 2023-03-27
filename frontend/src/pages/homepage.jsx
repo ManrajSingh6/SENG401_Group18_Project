524ddd364c1b0320a61c655c01d7bad278b5abd0
Manraj Singh <manraj.singh@ucalgary.ca>
2023-03-27 00:26:41 -0600

    revert homepage.jsx

--- frontend/src/pages/homepage.jsx
+++ frontend/src/pages/homepage.jsx
@@ -19,7 +19,7 @@ function Homepage(){
 
     // get all thread information and user information
     useEffect(() => {
-        fetch(`${import.meta.env.REACT_APP_API_URL}/threads/getallthreads`).then(res => {
+        fetch(`${process.env.REACT_APP_API_URL}/threads/getallthreads`).then(res => {
             res.json().then(resData => {
                 setAllThreads(resData.threads);
                 setPopularUsers(resData.users);

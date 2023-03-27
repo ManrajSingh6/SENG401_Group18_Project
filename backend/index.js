c8454bd69a6a585623ec34e02925f4ed5a89ab1c
Manraj Singh <manraj.singh@ucalgary.ca>
2023-03-27 00:20:41 -0600

    update CORS origin.

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use('/uploads', express.static(__dirname + '/uploads'));
 
 app.use(express.json());
-app.use(cors({credentials:true,origin:'http://localhost:3000'}));
+app.use(cors({credentials:true,origin:'https://seng401-theloop.vercel.app/'}));
 app.use('/threads',threadHandler);
 app.use('/users',userHandler);
 app.use('/posts',postHandler);
